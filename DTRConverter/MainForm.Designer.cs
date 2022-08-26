f3e1d94c7b9cb96a81f7b37be3b285e7ddb5d68d
Kiryuumaru <kiryuumaru@outlook.com>
2022-08-27 00:19:10 +0800

    Added assembly info

--- DTRConverter/MainForm.Designer.cs
+++ DTRConverter/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.textBoxFileName = new System.Windows.Forms.TextBox();
             this.buttonBrowse = new System.Windows.Forms.Button();
@@ -110,6 +111,7 @@
             this.Controls.Add(this.buttonBrowse);
             this.Controls.Add(this.textBoxFileName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
